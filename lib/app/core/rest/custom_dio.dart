44880321db51cfbbd16a9a24129c75398f1da8df
Medeiros <hsymetry@gmail.com>
2022-10-13 12:03:19 -0300

    Create and implement auth interceptor using Dio

--- lib/app/core/rest/custom_dio.dart
+++ lib/app/core/rest/custom_dio.dart
@@ -1,22 +1,27 @@
 import 'package:dio/dio.dart';
 import 'package:dio/native_imp.dart';
 import 'package:world_cup_album_manager/app/core/config/env/env.dart';
+import 'package:world_cup_album_manager/app/core/rest/interceptors/auth_interceptor.dart';
 
 class CustomDio extends DioForNative {
+  final _authInterceptor = AuthInterceptor();
+
   CustomDio()
       : super(BaseOptions(
           baseUrl: Env.instance['backend_base_url'] ?? '',
           connectTimeout: 5000,
           receiveTimeout: 60000,
-        )) {
-    interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
-  }
+        ));
 
   CustomDio auth() {
+    interceptors.add(_authInterceptor);
+    interceptors.add(LogInterceptor(
+        requestBody: true, responseBody: true, requestHeader: true));
     return this;
   }
 
   CustomDio unAuth() {
+    interceptors.remove(_authInterceptor);
     return this;
   }
 }
---
+++ lib/app/core/rest/interceptors/auth_interceptor.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthInterceptor extends Interceptor {
+  @override
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    final sp = await SharedPreferences.getInstance();
+    final accessToken = sp.getString('accessToken');
+    options.headers['Authorization'] = 'Bearer $accessToken';
+
+    handler.next(options);
+    super.onRequest(options, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    if (err.response?.statusCode == 401) {
+      
+    } else {
+      handler.next(err); 
+    }
+    super.onError(err, handler);
+  }
+}
