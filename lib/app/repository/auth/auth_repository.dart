5402f994a538aebfa0cf774b8de4b938121ad5dd
Medeiros <hsymetry@gmail.com>
2022-10-12 23:42:01 -0300

    Add auth repository

--- lib/app/repository/auth/auth_repository.dart
+++ lib/app/repository/auth/auth_repository.dart
@@ -1,7 +1,7 @@
 import 'package:world_cup_album_manager/app/models/register_user_model.dart';
 
 abstract class AuthRepository {
-  Future<void> register(RegisterUserModel registerModel);
-  Future<void> login({required String email, required String password});
-  Future<void> logout(); 
+  Future register(RegisterUserModel registerModel);
+  Future login({required String email, required String password});
+  Future logout(); 
 }
--- lib/app/repository/auth/auth_repository_impl.dart
+++ lib/app/repository/auth/auth_repository_impl.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:dio/dio.dart';
 import 'package:world_cup_album_manager/app/core/exceptions/repository_exception.dart';
+import 'package:world_cup_album_manager/app/core/exceptions/unauthorized_excpetion.dart';
 import 'package:world_cup_album_manager/app/core/rest/custom_dio.dart';
 import 'package:world_cup_album_manager/app/models/register_user_model.dart';
 
@@ -13,8 +14,27 @@ class AuthRepositoryImpl implements AuthRepository {
   AuthRepositoryImpl({required this.dio});
 
   @override
-  Future<void> login({required String email, required String password}) {
-    throw UnimplementedError();
+  Future<void> login({required String email, required String password}) async {
+    try {
+      final result = await dio.post('/api/auth', data: {
+        'email': email,
+        'password': password,
+      });
+      final accessToken = result.data['access_token'];
+
+      if (accessToken == null) {
+        throw UnauthorizedExcpetion();
+      }
+
+      return accessToken;
+    } on DioError catch (e, s) {
+      log('Erro ao realizar login', error: e, stackTrace: s);
+      if (e.response?.statusCode == 401) {
+        throw UnauthorizedExcpetion();
+      } else {
+        throw RepositoryException(message: 'Erro ao realizar login');
+      }
+    }
   }
 
   @override
