ed78112265d94df98c77a5205a5c8463696a2f9b
Medeiros <hsymetry@gmail.com>
2022-10-14 12:35:10 -0300

    Implement logout on Home page

--- lib/app/pages/home/presenter/home_presenter_impl.dart
+++ lib/app/pages/home/presenter/home_presenter_impl.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:world_cup_album_manager/app/pages/home/view/home_view.dart';
 import 'package:world_cup_album_manager/app/repository/user/user_repository.dart';
 
@@ -13,17 +14,21 @@ class HomePresenterImpl implements HomePresenter {
   @override
   Future<void> getUserData() async {
     try {
+      _view.showLoader();
       final user = await userRepository.getMe();
       _view.updateUser(user);
-    } catch (e, s) {
+    } catch (e) {
       _view.error('Erro ao buscar dados do usuário');
-      print('Error: $e\nStackTrace: $s');
-      
     }
   }
 
   @override
-  Future<void> logout() async {}
+  Future<void> logout() async {
+    _view.showLoader();
+    final sp = await SharedPreferences.getInstance();
+    sp.clear();
+    _view.logoutSuccess();
+  }
 
   @override
   set view(HomeView view) => _view = view;
